0db411d55d08501682dca9aa54b4f295d9bae5de
Changbum97 <chb2005@naver.com>
2023-09-03 02:03:28 +0900

    feat: service 추가

--- src/main/java/com/wanted/notion/service/PageService.java
+++ src/main/java/com/wanted/notion/service/PageService.java
@@ -1,2 +1,17 @@
-package com.wanted.notion.service;public class PageService {
+package com.wanted.notion.service;
+
+import com.wanted.notion.domain.dao.PageDao;
+import com.wanted.notion.domain.dto.PageDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PageService {
+
+    private final PageDao pageDao;
+
+    public PageDto findById(Long pageId) {
+        return pageDao.findById(pageId);
+    }
 }
